e5d654d374476d36403201b7f5dfc889bcbad5a5
mYmd <m-yamada@f06.itscom.net>
2014-08-19 22:23:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@ struct Func	{
 		{ return a * 10000 + b * 100 + c; }
 };
 
-int main(int argc, char **argv)
+int main()
 {
 	using namespace std::placeholders;
-	using namespace my::placeholders;
+	using namespace mymd::placeholders;
 	Func f;
 	int a = 2014, b = 2, c = 3, result = 0;
 	//----------------------------------------------------------------------------------
 	//basic   基本
-	auto b1 = my::rbind(f, _1, _2, _3);
+	auto b1 = mymd::rbind(f, _1, _2, _3);
 	result = b1(a, b, c);
 	std::cout << "---- basic use" << std::endl;
 	std::cout << "a = " << a << ", b = " << b << ", c = " << c << ", result = " << result << std::endl;
 	std::cout << std::endl;
-	auto b1_1 = my::rbind(f, until(_3rd));
+	auto b1_1 = mymd::rbind(f, until(_3rd));
 	result = b1_1(a, b, c);
 	std::cout << "---- basic use(placeholders range)" << std::endl;
 	std::cout << "a = " << a << ", b = " << b << ", c = " << c << ", result = " << result << std::endl;
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 	//----------------------------------------------------------------------------------
 	a = 2014, b = 2, c = 10;
 	//preset default value     デフォルト値の設定
-	auto b2 = my::rbind(f, a, _1, _2nd = 11);
+	auto b2 = mymd::rbind(f, a, _1, _2nd = 11);
 	result = b2(b, c);
 	std::cout << "---- do not use default value" << std::endl;
 	std::cout << "a = " << a << ", b = " << b << ", c = " << c << ", result = " << result << std::endl;
@@ -43,7 +43,7 @@ int main(int argc, char **argv)
 	//----------------------------------------------------------------------------------
 	a = 2014, b = 4, c = 30;
 	//optional argument     引数省略
-	auto b3 = my::rbind(_1st, 2014, _2nd, _3rd);
+	auto b3 = mymd::rbind(_1st, 2014, _2nd, _3rd);
 	result = b3(Func(), b);
 	std::cout << "---- use default value of original functor for c(=1)" << std::endl;
 	std::cout << "b = " << b << ", c = " << c << ", result = " << result << std::endl;
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 	a = 2014, b = 2, c = 1;
 	std::string str("abcdefg");
 	//convert argument by yield method     yieldメソッドによる引数変換
-	auto b4 = my::rbind(&Func::memFun	,
+	auto b4 = mymd::rbind(&Func::memFun	,
 			    &f			,
 			    2014		,
 			    _1			,
